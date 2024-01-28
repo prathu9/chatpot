abda93648e91af445f4d6794d1a13821a047b9cf
prathamesh sawant <30414105+prathu9@users.noreply.github.com>
2024-01-28 23:41:01 +0530

    populate users in chat while fetching

--- backend/controllers/chatController.js
+++ backend/controllers/chatController.js
@@ -50,12 +50,21 @@ const accessChat = asyncHandler(async (req, res) => {
 const fetchChats = asyncHandler(async (req, res) => {
     try{
         Chat.find({users: {$elemMatch: {$eq: req.user._id}}})
-            .then((result) => {
-                res.send(result);
+            .populate("users", "-password")
+            .populate("groupAdmin", "-password")
+            .populate("latestMessage")
+            .sort({updatedAt: -1})
+            .then(async (results) => {
+                results = await User.populate(results,{
+                    path: "latestMessage.sender",
+                    select: "name pic email",
+                });
+                res.status(200).send(results);
             })
     }
-    catch(err){
-
+    catch(error){
+        res.status(400);
+        throw new Error(error.message);
     }
 })
 
