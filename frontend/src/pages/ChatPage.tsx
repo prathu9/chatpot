7e1767e0a7a1d986e7300f036c68f8384ea8b6f2
prathamesh sawant <30414105+prathu9@users.noreply.github.com>
2024-01-31 10:34:27 +0530

    fixed typescript error in chats page

--- frontend/src/pages/ChatPage.tsx
+++ frontend/src/pages/ChatPage.tsx
@@ -1,8 +1,13 @@
 import axios from "axios";
 import {useEffect, useState} from "react";
 
+type ChatType = {
+  _id: string,
+  chatName: string
+}
+
 const ChatPage = () => {
-    const [chats, setChats] = useState([]);
+    const [chats, setChats] = useState<ChatType[]>([]);
 
     const fetchChats = async () => {
         const {data} = await axios.get("http://localhost:5000/api/chats");
