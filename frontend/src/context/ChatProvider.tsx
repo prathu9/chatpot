7abd4a1527ad35b0ba6d26decff4da04edf81cf8
prathamesh sawant <30414105+prathu9@users.noreply.github.com>
2024-02-02 20:03:43 +0530

    added chats and selected chats in context

--- frontend/src/context/ChatProvider.tsx
+++ frontend/src/context/ChatProvider.tsx
@@ -1,47 +1,80 @@
-import { ReactNode, createContext, useContext, useEffect, useState } from "react";
+import {
+  ReactNode,
+  createContext,
+  useContext,
+  useEffect,
+  useState,
+} from "react";
 import { useNavigate } from "react-router-dom";
 
-type User = {name: string,
-    email: string,
-    pic: string,
-    token: string,
-    _id: string}
+export type User = {
+  name: string;
+  email: string;
+  pic: string;
+  token: string;
+  _id: string;
+};
+export type ChatType = {
+  _id: string;
+  chatName: string;
+  isGroupChat: boolean;
+  users: User[];
+};
 
 type ChatContextValueType = {
-   user: User | null,
-   //setUser: React.Dispatch<React.SetStateAction<User | null>> 
-}
+  user: User | null;
+  setUser: React.Dispatch<React.SetStateAction<User | null>>;
+  selectedChat: ChatType | null;
+  setSelectedChat: React.Dispatch<React.SetStateAction<ChatType | null>>;
+  chats: ChatType[];
+  setChats: React.Dispatch<React.SetStateAction<ChatType[]>>;
+};
 
 const ChatContext = createContext<ChatContextValueType | null>(null);
 
 type ChatProviderProps = {
-    children: ReactNode
-}
+  children: ReactNode;
+};
 
-const ChatProvider = ({children}: ChatProviderProps) => {
-    const [user, setUser] = useState<User | null>(null);
-    const navigate = useNavigate();
+const ChatProvider = ({ children }: ChatProviderProps) => {
+  const [user, setUser] = useState<User | null>(null);
+  const [selectedChat, setSelectedChat] = useState<ChatType | null>(null);
+  const [chats, setChats] = useState<ChatType[]>([]);
+  const navigate = useNavigate();
 
-    useEffect(() => {
-        const userInfo = localStorage.getItem("userInfo");
-        if(userInfo){
-            const parsedUserInfo = JSON.parse(userInfo);
-            setUser(parsedUserInfo);
-        }
-        else{
-            navigate("/");
-        }
-    }, [navigate])
-    
-    return(
-        <ChatContext.Provider value={{user}}>
-            {children}
-        </ChatContext.Provider>
-    )
-}
+  useEffect(() => {
+    const userInfo = localStorage.getItem("userInfo");
+    if (userInfo) {
+      const parsedUserInfo = JSON.parse(userInfo);
+      setUser(parsedUserInfo);
+    } else {
+      navigate("/");
+    }
+  }, [navigate]);
+
+  return (
+    <ChatContext.Provider
+      value={{ user, setUser, selectedChat, setSelectedChat, chats, setChats }}
+    >
+      {children}
+    </ChatContext.Provider>
+  );
+};
 
 export const ChatState = () => {
-    return useContext(ChatContext);
-}
+  const chatInfo = useContext(ChatContext);
+  if (chatInfo) {
+    return { ...chatInfo };
+  } else {
+    return {
+      user: null,
+      setUser: null,
+      selectedChat: null,
+      setSelectedChat: null,
+      chats: [],
+      setChats: null
+    };
+  }
+};
 
 export default ChatProvider;
