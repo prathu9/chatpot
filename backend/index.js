08d7a74690e9507d24b57cce403d9a69428620ae
prathamesh sawant <30414105+prathu9@users.noreply.github.com>
2024-02-11 20:23:45 +0530

    added socket io in backend

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const data = require("./data/data");
 const cors = require("cors");
 const dotenv = require("dotenv");
+const socket = require("socket.io");
 const connectDB = require("./config/db");
 const userRoutes = require("./routes/userRoutes");
 const chatRoutes = require("./routes/chatRoutes");
@@ -41,7 +42,40 @@ app.get("/chat/:id", (req,res) => {
     res.send(findChat);
 })
 
-app.listen(port, () => {
+const server = app.listen(port, () => {
     console.log(`server running on port ${port}`);
     console.log(`Open: http://localhost:${port}`)
 })
+
+const io = socket(server, {
+    pingTimeout: 60000,
+    cors: {
+        origin: "http://localhost:5173"
+    }
+})
+
+io.on("connection", (socket) => {
+    console.log("Connected to socket.io");
+
+    socket.on("setup", (userData) => {
+        socket.join(userData._id);
+        socket.emit("connected");
+    });
+
+    socket.on('join chat', (room) => {
+        socket.join(room);
+        console.log("User joined Room: "+room);
+    });
+
+    socket.on("new message", (newMessageRecieved) => {
+        let chat = newMessageRecieved.chat;
+
+        if(!chat.users) return console.log("Chat.users not defined");
+
+        chat.users.forEach((user) => {
+            if(user._id === newMessageRecieved.sender._id) return;
+
+            socket.in(user._id).emit("message recieved", newMessageRecieved);
+        }); 
+    })
+});
