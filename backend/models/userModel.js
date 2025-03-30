0a5c7ea4f9aada81d575bcdf25678d4ec849a3cf
prathamesh sawant <30414105+prathu9@users.noreply.github.com>
2025-03-30 21:28:56 +0530

    added node version file for render deployment

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -15,7 +15,8 @@ const userSchema = mongoose.Schema({
 });
 
 userSchema.methods.matchPassword = async function(enteredPassword){
-    return await bcrypt.compare(enteredPassword, this.password);
+    const result = await bcrypt.compare(enteredPassword, this.password);
+    return result;
 }
 
 userSchema.pre('save', async function(next){
